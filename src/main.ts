d1b8255ce0cd8a79221c1c58115252203644522d
Davi Horner Hoe de Castro <davihhcastro@gmail.com>
2022-08-17 22:37:26 -0300

    comment main

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ import { AppModule } from './app.module';
 const prisma = new PrismaClient();
 
 async function bootstrap() {
-  const tarefa = await prisma.tarefa.create({
+  /*const tarefa = await prisma.tarefa.create({
     data:{
       nome: "Meu primeiro teste nome",
       desc: "Meu Primeiro teste desc",
       prazo: "2022-01-20T12:01:30.543Z",
       concluido: false,
     }
-  })
+  })*/
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
